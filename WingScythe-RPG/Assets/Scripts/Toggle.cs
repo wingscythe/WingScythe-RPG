f8ebf9a8ed00c8142025d0ac3bbacb3153944586
Jeffrey Weng <jweng8@binghamton.edu>
2019-06-15 14:45:22 -0400

    toggling works

--- WingScythe-RPG/Assets/Scripts/Toggle.cs
+++ WingScythe-RPG/Assets/Scripts/Toggle.cs
@@ -1,12 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Toggle : MonoBehaviour
-{   
-    void onClick()
+{
+    public Button toggle;
+    public CanvasGroup attack;
+    public CanvasGroup moves;
+    private void Start()
     {
-
+        toggle.onClick.AddListener(TaskOnClick);
     }
+
+    private void TaskOnClick()
+    {
+        if (attack.alpha == 1f || moves.alpha == 0f)
+        {
+            attack.alpha = 0f;
+            attack.interactable = false;
+            attack.blocksRaycasts = false;
+            moves.alpha = 1f;
+            moves.interactable = true;
+            moves.blocksRaycasts = true;
+        }
+        else if(moves.alpha == 1f || attack.alpha == 0f)
+        {
+            moves.alpha = 0f;
+            moves.interactable = false;
+            moves.blocksRaycasts = false;
+            attack.alpha = 1f;
+            attack.interactable = true;
+            attack.blocksRaycasts = true;
+        }
+    }
+
 }
