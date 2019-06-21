e232748bbd19ed74587b062ca4f85acdf9c8e6cf
zhenga6 <zhenga6@bxscience.edu>
2019-06-21 00:02:02 -0400

    Working on monster movement
    Monster is capable of movementing and detecting colliders.

--- WingScythe-RPG/Assets/Testing/scripts/MonsterMovement.cs
+++ WingScythe-RPG/Assets/Testing/scripts/MonsterMovement.cs
@@ -7,19 +7,28 @@ public class MonsterMovement : MonoBehaviour
 
     public Animator anim;
     public Rigidbody2D rb;
-    public int[] moveplace = new int[4];
 
     // Start is called before the first frame update
     void Start()
     {
         anim = this.gameObject.GetComponent<Animator>();
         rb = this.gameObject.GetComponent<Rigidbody2D>();
+        StartCoroutine(movementDelay(1.0f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        idle_Movement();
+        //idle_Movement();
+    }
+
+    IEnumerator movementDelay(float time)
+    {
+        while (this.gameObject.activeSelf)
+        {
+            idle_Movement();
+            yield return new WaitForSeconds(time);
+        }
     }
 
     void idle_Movement()
@@ -29,56 +38,43 @@ public class MonsterMovement : MonoBehaviour
         RaycastHit2D left = Physics2D.Raycast(transform.position, new Vector2(-1, 0));
         RaycastHit2D right = Physics2D.Raycast(transform.position, new Vector2(1, 0));
 
-        if (up.collider != null)
+        float randPercent = Random.Range(0f, 100f);
+        if (up.collider != null && randPercent < 20)
         {
-            moveplace[0] = 1;
+            this.gameObject.transform.Translate(new Vector2(0, 0.5f));
+            anim.SetBool("Walk", true);
         }
-        else
+        else if (down.collider != null && 20 <= randPercent && randPercent < 40)
         {
-            moveplace[0] = 0;
+            this.gameObject.transform.Translate(new Vector2(0, -0.5f));
+            anim.SetBool("Walk", true);
         }
-
-        if (down.collider != null)
+        else if (left.collider != null && 40 <= randPercent && randPercent < 60)
         {
-            moveplace[1] = 1;
-        }
-        else
-        {
-            moveplace[1] = 0;
-        }
-
-        if (left.collider != null)
-        {
-            moveplace[2] = 1;
-        }
-        else
-        {
-            moveplace[2] = 0;
-        }
-
-        if (right.collider != null)
-        {
-            moveplace[3] = 1;
-        }
-        else
-        {
-            moveplace[3] = 0;
-        }
-
-        int counter = 0;
-
-        for (int i = 0; i < moveplace.Length; i++)
-        {
-            if (moveplace[i] == 0)
+            this.gameObject.transform.Translate(new Vector2(-0.5f, 0));
+            anim.SetBool("Walk", true);
+            if (this.gameObject.GetComponent<SpriteRenderer>().flipX == true)
             {
-                counter++;
+                this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
             }
         }
-        double percent = 100.0 / counter;
-
-        float addon = Random.Range(0f, 100f);
-
+        else if (right.collider != null && 60 <= randPercent && randPercent < 80)
+        {
+            this.gameObject.transform.Translate(new Vector2(0.5f, 0));
+            anim.SetBool("Walk", true);
+            if (this.gameObject.GetComponent<SpriteRenderer>().flipX == false)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+        else if (randPercent >= 90 && randPercent < 100)
+        {
+            anim.SetBool("Walk", false);
+            anim.ResetTrigger("Attack");
+        }
+        else {
+            randPercent = Random.Range(0f, 100f);
+        }
         
     }
-
 }
