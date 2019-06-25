d2c12801f25b9c6a026f4bfc6acc6f1947429f02
Jeffrey Weng <jweng8@binghamton.edu>
2019-06-25 11:54:20 -0400

    sw

---
+++ WingScythe-RPG/Assets/Scripts/SW.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SW : MonoBehaviour
+{
+    public Button sw;
+    public GameObject hero;
+    public GameObject weapon1;
+    public GameObject weapon2;
+
+    public GameObject weapon;
+    public Weapon ws;
+    public bool slot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapon1 = hero.transform.GetChild(0).gameObject;
+        weapon2 = hero.transform.GetChild(1).gameObject;
+
+        slot = true;
+        weapon = weapon1;
+        ws = weapon.GetComponent<Weapon>();
+        sw.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TaskOnClick()
+    {
+        if (slot)
+        {
+            weapon = weapon2;
+        }
+        else
+        {
+            weapon = weapon1;
+        }
+        ws = weapon.GetComponent<Weapon>();
+        slot = !slot;
+        swapWeapon();
+    }
+
+    public void swapWeapon()
+    {
+        weapon1.SetActive(slot);
+        weapon2.SetActive(!slot);
+    }
+}
