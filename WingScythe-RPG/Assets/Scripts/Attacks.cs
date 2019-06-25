0283fba10fa64cff1dec1715403f15aafc1534eb
Jeffrey Weng <jweng8@binghamton.edu>
2019-06-25 15:07:28 -0400

    weapon moves wip

--- WingScythe-RPG/Assets/Scripts/Attacks.cs
+++ WingScythe-RPG/Assets/Scripts/Attacks.cs
@@ -14,40 +14,46 @@ public class Attacks : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        wp = getWeapon();
         attack.onClick.AddListener(Basic);
         spAtk1.onClick.AddListener(SpAtk1);
         spAtk2.onClick.AddListener(SpAtk2);
         spAtk3.onClick.AddListener(SpAtk3);
-        getWeapon();
     }
 
-    void getWeapon()
+    Weapon getWeapon()
     {
-        wp = hero.transform.GetChild(0).gameObject.GetComponent<Weapon>();
+
+        return hero.transform.GetChild(0).gameObject.GetComponent<Weapon>();
     }
 
     public void Basic()
     {
+        getWeapon();
         wp.Basic_Attack();
     }
 
     public void SpAtk1()
     {
+        getWeapon();
         PlayerSpecialAttack(1);
     }
 
     public void SpAtk2()
     {
+        getWeapon();
         PlayerSpecialAttack(2);
     }
 
     public void SpAtk3()
     {
+        getWeapon();
         PlayerSpecialAttack(3);
     }
 
     private void PlayerSpecialAttack(int index)
     {
+        getWeapon();
         wp.Special_Attack(index);
     }
 }
