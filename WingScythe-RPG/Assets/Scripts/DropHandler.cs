78be105879b1b26485950dd1f717b8f8d675d131
Jeffrey Weng <jweng8@binghamton.edu>
2019-07-31 18:00:37 -0400

    drop handle wip

--- WingScythe-RPG/Assets/Scripts/DropHandler.cs
+++ WingScythe-RPG/Assets/Scripts/DropHandler.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
+/*
+ * This class allows the items to be dragged and dropped(Inventory)
+ *
+*/
 public class DropHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
     //drag variables
     public Vector2 start;
+    public Vector2 switchpos;
     public Transform parent;
     public GameObject pulled;
+    public Image img;
 
     public void Start()
     {
         pulled = item;
+        switchpos = img.transform.position;
+        Debug.Log(switchpos);
     }
     public GameObject item
     {
@@ -25,29 +34,41 @@ public class DropHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
             return null;
         }
     }
+    //gets item getting pulled and position
     public void OnBeginDrag(PointerEventData eventData)
     {
         pulled = gameObject;
         start = transform.position;
         parent = transform.parent;
+        Debug.Log(parent);  
     }
-
+    //allows getting dragged
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = Input.mousePosition;
     }
 
+    //returns back to original if cannot be swapped
     public void OnEndDrag(PointerEventData eventData)
     {
-        pulled = null;
-        if(transform.position!= parent.position)transform.position = start;
+        if (transform.position != parent.position)
+        {
+            if (transform.position.x<img.transform.position.x+2)
+            {
+                pulled = null;
+                transform.position = switchpos;
+                img.transform.position = start;
+            }
+            transform.position = start;
+        }
 
     }
 
     //swapping wip
     public void OnDrop(PointerEventData eventData)
     {
-        if(!item)pulled.transform.SetParent(transform);
+       
+        Debug.Log(this.transform.position);
     }
     
 
